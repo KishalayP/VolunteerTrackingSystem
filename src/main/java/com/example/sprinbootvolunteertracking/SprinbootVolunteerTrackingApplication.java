23cd2fd394925173b352611c67910c19961c36d5
Subramaniyam-cpu <2022cfse004@wilp.bits-pilani.ac.in>
2023-03-18 06:53:47 -0800

    Something I dont understand

---
+++ src/main/java/com/example/sprinbootvolunteertracking/SprinbootVolunteerTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprinbootvolunteertracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootVolunteerTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinbootVolunteerTrackingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sprinbootvolunteertracking/SprinbootVolunteerTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinbootvolunteertracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootVolunteerTrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
